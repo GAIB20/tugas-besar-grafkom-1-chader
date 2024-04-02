b7f49fece0ce925a5f8e875f4f8808a5e50ab75a
Addin Munawwar <91366027+moonawar@users.noreply.github.com>
2024-04-03 01:14:05 +0700

    feat: the random rectangle

--- src/main.ts
+++ src/main.ts
@@ -58,11 +58,11 @@ function main() {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     const positions = [
         10, 20,
-        400, 20,
+        80, 20,
         10, 30,
         10, 30,
-        400, 20,
-        400, 30,
+        80, 20,
+        80, 30,
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
@@ -78,26 +78,52 @@ function main() {
     // Use the program
     gl.useProgram(program);
 
-    // Turn on the attribute
-    const resolutionUniformLocation = gl.getUniformLocation(program, 'u_Resolution');
-    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+    // Tell the attribute how to get data out of the buffer
     gl.enableVertexAttribArray(positionAttributeLocation);
-
-    // Bind the position buffer
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-    // Tell the attribute how to get data out of the buffer
-    const size = 2;             // 2 components per iteration
-    const type = gl.FLOAT;      // the data is 32bit floats
-    const normalize = false;    // don't normalize the data
-    const stride = 0;           // 0 = move forward size * sizeof(type) each iteration to get the next position
-    const offset = 0;           // start at the beginning of the buffer
+    var size = 2;             // 2 components per iteration
+    var type = gl.FLOAT;      // the data is 32bit floats
+    var normalize = false;    // don't normalize the data
+    var stride = 0;           // 0 = move forward size * sizeof(type) each iteration to get the next position
+    var offset = 0;           // start at the beginning of the buffer
     gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
 
-    // Draw
-    const primitiveType = gl.TRIANGLES;
-    const count = 6;
+    // Uniforms
+    const resolutionUniformLocation = gl.getUniformLocation(program, 'u_Resolution');
+    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+
+    const colorUniformLocation = gl.getUniformLocation(program, 'u_Color');
+
+    setRectangle(gl, randomInt(300), randomInt(300), randomInt(300), randomInt(300));
+
+    gl.uniform4f(colorUniformLocation, Math.random(), Math.random(), Math.random(), 1);
+
+    // Draw the rectangle
+    var primitiveType = gl.TRIANGLES;
+    var offset = 0;
+    var count = 6;
     gl.drawArrays(primitiveType, offset, count);
+}
+
+function randomInt(range : number) {
+    return Math.floor(Math.random() * range);
+}
+
+function setRectangle(gl : WebGL2RenderingContext, x : number, y : number, width : number, height : number) {
+    const x1 = x;
+    const x2 = x + width;
+    const y1 = y;
+    const y2 = y + height;
+
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+        x1, y1,
+        x2, y1,
+        x1, y2,
+        x1, y2,
+        x2, y1,
+        x2, y2,
+    ]), gl.STATIC_DRAW);
 }
 
 main();
